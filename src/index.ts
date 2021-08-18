828dc5290c2688d6f0ef6adec8f9e781ca18edd9
iyegoroff <iegoroff@gmail.com>
2021-08-18 21:24:35 +0300

    added useArgPipe

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { pipe } from 'pipe-ts'
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 
 type UnknownFunction = (...params: readonly unknown[]) => unknown
 
@@ -80,4 +80,82 @@ export function usePipe<
 
 export function usePipe(...fns: readonly [UnknownFunction]): UnknownFunction {
   return useMemo(() => pipe(...fns), fns)
+}
+
+export function useArgPipe<A, B>(arg: A, ab: (a: A) => B): () => B
+export function useArgPipe<A, B, C>(
+  arg: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C
+): () => C
+export function useArgPipe<A, B, C, D>(
+  arg: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D
+): () => D
+export function useArgPipe<A, B, C, D, E>(
+  arg: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E
+): () => E
+export function useArgPipe<A, B, C, D, E, F>(
+  arg: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F
+): () => F
+export function useArgPipe<A, B, C, D, E, F, G>(
+  arg: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+  fg: (f: F) => G
+): () => G
+export function useArgPipe<A, B, C, D, E, F, G, H>(
+  arg: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+  fg: (f: F) => G,
+  gh: (g: G) => H
+): () => H
+export function useArgPipe<A, B, C, D, E, F, G, H, I>(
+  arg: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+  fg: (f: F) => G,
+  gh: (g: G) => H,
+  hi: (h: H) => I
+): () => I
+export function useArgPipe<A, B, C, D, E, F, G, H, I, J>(
+  arg: A,
+  ab: (a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+  fg: (f: F) => G,
+  gh: (g: G) => H,
+  hi: (h: H) => I,
+  ij: (i: I) => J
+): () => J
+
+export function useArgPipe(
+  arg: unknown,
+  ...fns: readonly [UnknownFunction]
+): UnknownFunction {
+  const getArg = useCallback(() => arg, [arg])
+  return useMemo(() => pipe(getArg, ...fns), [getArg, fns])
 }
